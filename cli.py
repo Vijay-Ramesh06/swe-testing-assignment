4b673baa4c4eee18a494ec0ad40c3a3d68602a2f
Vijay-Ramesh06 <vijay.ramesh@edu.rtu.lv>
2026-03-05 21:44:40 +0200

    feat: add CLI interface for calculator

---
+++ cli.py
@@ -0,0 +1,15 @@
+from calculator import Calculator
+
+calc = Calculator()
+
+def calculate(a, op, b):
+    if op == "+":
+        return calc.add(a, b)
+    elif op == "-":
+        return calc.subtract(a, b)
+    elif op == "*":
+        return calc.multiply(a, b)
+    elif op == "/":
+        return calc.divide(a, b)
+    else:
+        raise ValueError("Invalid operator")
